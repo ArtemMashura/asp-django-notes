cc2b07e2c60d9bc1bc92ef8c16f6cecfdda9d547
ArtemMashura <Masu_ih91@student.itstep.org>
2024-04-27 02:01:29 +0300

    Добавьте файлы проекта.

---
+++ asp-django notes/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using asp_django_notes.Models;
+
+namespace asp_django_notes.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<Notes> Notes { get; set; } = default!;
+    }
+}
---
+++ asp-django notes/Models/Notes.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace asp_django_notes.Models
+{
+    public class Notes
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Content { get; set; }
+
+    }
+}
